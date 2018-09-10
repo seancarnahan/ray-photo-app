4c903beb2b1a8a474c57af7a314c595d8d494c36
seancarnahan <Sean.Carnahan5@gmail.com>
2018-09-10 16:38:06 -0700

    CRE-GALLERY: safe commit main backup

--- src/js/components/contact.jsx
+++ src/js/components/contact.jsx
@@ -14,19 +14,19 @@ class Contact extends Component {
           Contact Me
         </h1>
         <form>
-          <div class="emailBox">
-            <label for="emailAddress">Your email address</label><br/>
+          <div className="emailBox">
+            <label for ="emailAddress">Your email address</label><br/>
             <input id="emailAddress" type="email" size="64" maxLength="64" required
               placeholder="username@beststartupever.com" pattern=".+@beststartupever.com"
               title="Please provide only a Best Startup Ever corporate email address"/>
           </div>
 
-          <div class="messageBox">
+          <div className="messageBox">
             <label for="message">Request</label><br/>
             <textarea id="message" cols="80" rows="8" required
              placeholder="My shoes are too tight, and I have forgotten how to dance."></textarea>
           </div>
-            <input type="submit" value="Send Request"/ >
+          <input type="submit" value="Send Request"/>
         </form>
       </Grid>
     );
