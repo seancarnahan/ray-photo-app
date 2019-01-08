45308985ee9fecbc912a8263ccf68e3a1e284301
seancarnahan <Sean.Carnahan5@gmail.com>
2019-01-08 14:00:25 -0800

    RPA-6: fixed about me navbar

--- src/routes/js/components/aboutMeNavbar.js
+++ src/routes/js/components/aboutMeNavbar.js
@@ -21,13 +21,13 @@ class AboutMeNavbar extends Component {
             <NavItem eventKey={1} componentClass={Link} href="/" to="/">
               Home
             </NavItem>
-            <NavItem eventKey={2} componentClass={Link} href="/gallery" to="/gallery">
+            <NavItem eventKey={2} componentClass={Link} href="/travel/bigsur" to="/travel/bigsur">
               Travel
             </NavItem>
-            <NavItem eventKey={3} componentClass={Link} href="/gallery" to="/gallery">
+            <NavItem eventKey={3} componentClass={Link} href="/sunsets" to="/sunsets">
               Sunsets
             </NavItem>
-            <NavItem eventKey={4} componentClass={Link} href="/gallery" to="/gallery" >
+            <NavItem eventKey={4} componentClass={Link} href="/friends" to="/friends" >
               Friends
             </NavItem>
             <NavItem eventKey={5} componentClass={Link} href="/about-me" to="/about-me" >
